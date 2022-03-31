104290f22f392e7e9449f5b4e1381577600d3f3b
pixelogicsolutions <91719168+pixelogicsolutions@users.noreply.github.com>
2022-04-01 01:55:36 +0530

    Update Apidata.js

--- src/Api/Apidata.js
+++ src/Api/Apidata.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const Data = fetch('https://randomuser.me/api/?results=10').then(res=>res.json()).then(data=>data.results)
+// const Data = fetch('https://randomuser.me/api/?results=10').then(res=>res.json()).then(data=>data.results)
 
 
 
